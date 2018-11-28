e12a41635b25a9fb08330d4751daad8eeaeeb5e5
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2018-11-28 15:35:39 +0000

    docking station now returns a bike object, this object responds to working - upto step 11

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,15 @@ describe DockingStation do
     expect(DockingStation.new).to respond_to(:release_bike)
   end
 
+  it "returns a Bike Object on release_bike method" do
+     station = DockingStation.new
+     bike = station.release_bike
+     expect(bike).to be_instance_of(Bike)
+  end
+
+  it "the returned bike Object should respond to working method" do
+    station = DockingStation.new
+    bike = station.release_bike
+    expect(bike).to respond_to(:working)
+  end
 end
